71b9505a5900ab3b1b5b0448e301e992e3c00f5f
anayinfowind <anay.infowind@gmail.com>
2020-10-06 12:13:29 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -17,7 +17,7 @@
 /**
  * Main functions of the plugin.
  *
- * @package auth_leeloolxp_tracking_sso
+ * @package local_leeloolxp_web_login_tracking
  * @author Leeloo LXP <info@leeloolxp.com>
  * @copyright  2020 Leeloo LXP (https://leeloolxp.com)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
